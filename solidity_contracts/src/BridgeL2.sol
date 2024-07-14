3e4fccb9577fa30821a2c5d12278c67bcbf3394a
Thomas Butler <trbiv@proton.me>
2024-07-14 07:00:45 -0500

    updated bridge l2

--- solidity_contracts/src/BridgeL2.sol
+++ solidity_contracts/src/BridgeL2.sol
@@ -2,10 +2,13 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 import "kakarot-lib/CairoLib.sol";
-import "../lib/openzeppelin-contracts/contracts/interfaces/IERC20.sol";
+
+interface IERC20L2 {
+    function mint(address account, uint256 amount) external;
+}
 
 contract BridgeL2 {
-    IERC20 private _erc20;
+    IERC20L2 private _erc20L2;
 
     // @notice Sends a message to L1.
     // @dev Uses the Cairo Precompiles mechanism to invoke a the send_message_to_l1 syscall
@@ -19,7 +22,7 @@ contract BridgeL2 {
         address l2ERC20Address,
         uint256 amount
     ) external {
-        _erc20 = IERC20(l2ERC20Address);
-        _erc20.transfer(recipient, amount);
+        _erc20L2 = IERC20L2(l2ERC20Address);
+        _erc20L2.mint(recipient, amount);
     }
 }
